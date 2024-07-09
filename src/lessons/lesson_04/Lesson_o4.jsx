b24554274e390b9c25d9ce1b7e1e777517f310d7
alisaTonga <tongalise@gmail.com>
2024-07-09 23:13:20 +0200

    Hw5 task 1

--- src/lessons/lesson_04/Lesson_o4.jsx
+++ src/lessons/lesson_04/Lesson_o4.jsx
@@ -1,21 +1,25 @@
 import './lesson_04.css'
 
 import { forceUsers } from './data'
+
 export default function Lesson_04(){
     return(
         <div className="lesson_container">
-            <h5>Lesson 04 </h5>
+            <h5 id='title'>Lesson 04 </h5>
+            <div className='cards'> 
             {forceUsers.map((hero,index) => (
-                <div key={index}>
+                <div key={index} id='card' className={`card ${hero.isDark ? 'dark' : 'light'}`}>
                     <h4>{hero.name}</h4>
                     <h5>Age: {hero.age}</h5>
-                    <img width={200} src={hero.image} alt="" />
+                    <img id='photo' width={200} src={hero.image} alt="" />
                     <h5>Lightsaber colors: {' '}
                         {hero.lightsaberColors.map(color => (
-                            <span>{color} </span>
+                            <ol>{color}</ol>
                         ))}</h5>
+
                 </div>
             ))}
+            </div>
         </div>
     )
 }
