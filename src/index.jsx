db498e1a6d7abc3c76c44295092b67c9cb4696f6
alisaTonga <tongalise@gmail.com>
2024-07-10 00:07:55 +0200

    Hw5 task 1.1

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,8 @@ import Lesson2 from './lessons/lesson2/Lesson2';
 import Counter from './components/counter/Counter';
 import Homework04 from './homework/homework04/Homework04';
 import Lesson_04 from './lessons/lesson_04/Lesson_o4';
+import Card from './components/cards/card';
+import Homework5 from './homework/homework05/homework5';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,6 +19,7 @@ root.render(
     {/* <Homework02 /> */}
     {/* <Lesson2 /> */}
     {/* <Homework04 /> */}
-    <Lesson_04 />
+    {/* <Lesson_04 /> */}
+      <Homework5 />
   </>
 );
